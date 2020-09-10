c8451b5d2b4c0bc0ea82031eb05ce383963eb30f
Mike Bergman <mike@mkbergman.com>
2020-09-09 22:01:44 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -4,19 +4,25 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cowpoke-mkbergman",
-    version="0.0.1",
-    author="Michael K Bergman",
-    author_email="mike@mkbergman.com",
+    name='cowpoke',
+    version='0.0.1',
+    author='Michael K Bergman',
+    author_email='mike@mkbergman.com',
     description="Code in support of the 'Cooking with Python and KBpedia' (CWPK) series",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Cognonto/cowpoke",
+    long_description_content_type='text/markdown',
+	keywords='KBpedia, knowledge graph, build, extract', 
+    url='https://github.com/Cognonto/cowpoke',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
+	project_urls={ 
+        'CWPK blog series': 'https://www.mkbergman.com/cooking-with-python-and-kbpedia/',
+        'CWPK Jupyter notebooks': 'https://github.com/Cognonto/CWPK',
+        'KBpedia': 'https://kbpedia.org/',
+        'Source': 'https://github.com/Cognonto/cowpoke/'}
 )
