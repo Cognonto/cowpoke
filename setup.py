e676b12e26d26a4f56e5eeac71273407ec89c15a
Mike Bergman <mike@mkbergman.com>
2025-03-10 14:23:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='cowpoke',
-    version='0.0.1',
+    version='0.0.2',
     author='Michael K Bergman',
     author_email='mike@mkbergman.com',
     description="Code in support of the 'Cooking with Python and KBpedia' (CWPK) series",
@@ -14,7 +14,7 @@ setuptools.setup(
 	keywords='KBpedia, knowledge graph, build, extract', 
     url='https://github.com/Cognonto/cowpoke',
 #    packages=setuptools.find_packages(),
-    py_modules=['__init__', '__main__', 'build', 'config', 'extract', 'mapping', 'setup', 'stats', 'utils' 'visualize'],
+    py_modules=['__main__', 'build', 'config', 'extract', 'mapping', 'setup', 'stats', 'utils', 'visualize'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
